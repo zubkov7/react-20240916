ba26803e2a973e9637fda8b6ac020b21117a8ce8
azubkovd <zubkov.andrey.99@mail.ru>
2024-09-23 21:56:06 +0300

    lecture 3: components, useState

--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,8 @@
 import { createRoot } from "react-dom/client";
+import { App } from "./components/app/App";
 
 const rootElement = document.getElementById("root");
 
 const root = createRoot(rootElement);
 
-// root.render(
-//   createElement(
-//     "ul",
-//     {}, // { style: { color: "red" }, className: "someClass otherClass" },
-//     [1, 2, 3, 4, 5, 6].map((item) => createElement("li", {}, item))
-//   )
-// );
-
-root.render(
-  <ul>
-    {[1, 2, 3, 4, 5, 6].map((item) => (
-      <li>{item}</li>
-    ))}
-
-    <button
-      onClick={() => {
-        console.log("click");
-      }}
-    >
-      click
-    </button>
-  </ul>
-);
+root.render(<App title='Headphones app' />);
