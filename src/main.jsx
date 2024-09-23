48a40c7ed646366297da5a1f507667838c49aa3e
AlexanderGalt <alexander.galt@mail.ru>
2024-09-23 03:04:39 +0200

    feat: render restaurant

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import { createRoot } from "react-dom/client";
+import { restaurants } from "../materials/mock.js";
 
 const rootElement = document.getElementById("root");
 
@@ -12,18 +13,36 @@ const root = createRoot(rootElement);
 //   )
 // );
 
-root.render(
-  <ul>
-    {[1, 2, 3, 4, 5, 6].map((item) => (
-      <li>{item}</li>
-    ))}
 
-    <button
-      onClick={() => {
-        console.log("click");
-      }}
-    >
-      click
-    </button>
-  </ul>
+root.render(
+	restaurants.map(
+		restaurant => (
+			<div className="restaurant">
+				<h2 id={restaurant.id} className="restaurantName">
+					{restaurant.name}
+				</h2>
+				<h3>Меню</h3>
+				<ul>
+					{restaurant.menu.map(menuItem => (
+						<li
+							data-price={menuItem.price}
+							data-id={menuItem.id}
+							data-ingredients={menuItem.ingredients}
+						>{menuItem.name}</li>
+					))}
+				</ul>
+
+				<h3>Отзывы</h3>
+				<ul>
+					{restaurant.reviews.map(reviewsItem => (
+						<li
+							data-rating={reviewsItem.rating}
+							data-id={reviewsItem.id}
+							data-user={reviewsItem.user}
+						>{reviewsItem.text}</li>
+					))}
+				</ul>
+			</div >
+		)
+	)
 );
