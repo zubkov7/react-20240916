02088d6ca4de35df5ca66710b4fd204fca3d03b0
AlexanderGalt <alexander.galt@mail.ru>
2024-09-26 15:18:26 +0200

    refactor(TabsRestaurants.jsx):swap index with Id

--- src/components/Restaurant.jsx
+++ src/components/Restaurant.jsx
@@ -2,7 +2,6 @@ import { Menu } from "./Menu.jsx";
 import { Reviews } from "./Reviews.jsx";
 
 export function Restaurant({ restaurantData }) {
-	console.dir(restaurantData);
 	return ( // здесь условый рендеринг (restaurantData.name &&) не нужен, т.к. сюда всегда будут попадать "restaurantData", у которых есть имя, т.к. проверка на имя ресторана ужеесть в заголовке таба.
 		<div className="restaurant-item" data-id={restaurantData.id}>
 			<h2 className="restaurant-item__name">
--- src/components/TabsRestaurants.jsx
+++ src/components/TabsRestaurants.jsx
@@ -3,17 +3,18 @@ import { Restaurant } from "./Restaurant.jsx";
 import { useState } from 'react'
 
 export function TabsRestaurants() {
-	const startIndex = restaurants.findIndex(((restaurantItem) => restaurantItem.name)); // на случай если ресторан с индексом 0 будет без имени, т.е. невалидный. Иначе, если далить имя у первого ресторана, то первый рендер будет баганый (будет постой таб).
-	const [currIndex, setCurrIndex] = useState(startIndex);
+	const startRestaurantTab = restaurants.find((restaurantItem) => restaurantItem.name); // на случай если ресторан с индексом 0 будет без имени, т.е. невалидный. Иначе, если удалить имя у первого ресторана, то первый рендер будет баганый (будет постой таб).
+	const startId = startRestaurantTab.id;
+	const [currId, setCurrId] = useState(startId);
 
 	return !!restaurants.length && (
 		<div className="restaurants-tabs">
 			<div className="restaurants-tabs__header">
 				{
-					restaurants.map((restaurant, index) => !!restaurant.name && (
+					restaurants.map((restaurant) => !!restaurant.name && (
 						<div
-							onClick={event => setCurrIndex(event.target.dataset.index)}
-							data-index={index}
+							onClick={event => setCurrId(event.target.dataset.id)}
+							data-id={restaurant.id}
 							className="restaurants-tabs__title"
 						>{restaurant.name}</div>
 					))
@@ -21,9 +22,8 @@ export function TabsRestaurants() {
 			</div>
 
 			<div className="restaurants-tabs__body">
-				<Restaurant restaurantData={restaurants[currIndex]} />
+				<Restaurant restaurantData={restaurants.find((restaurant) => restaurant.id === currId)} />
 			</div>
-
 		</div>
 	)
 }
